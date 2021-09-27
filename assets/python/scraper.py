b59a0ba2d3f10d9e1caae1fd59fd12dfc07fc251
willjburns <willjburns80@gmail.com>
2021-09-27 10:44:07 -0700

    Lots of changes,  created template for each item's individual page

---
+++ assets/python/scraper.py
@@ -0,0 +1,11 @@
+
+
+# import library 
+#from bs4 import BeautifulSoup
+#import requests
+# Request to website and download HTML contents
+#url='https://stockx.com/air-jordan-4-retro-lightning-2021'
+#req=requests.get(url)
+#content=req.text
+
+#soup=BeautifulSoup(content)
